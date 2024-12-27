b8f73f5039226a3e776b1f4ec63a063a2ff7468b
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2024-12-27 18:47:57 +0200

    added keyboard listener

--- src/ui/button/Button.js
+++ src/ui/button/Button.js
@@ -1,11 +1,12 @@
 import { Assets, Container, Sprite, Text } from 'pixi.js'
 import { buttonStyle } from './buttonStyle.js'
-import { CONFIG } from '../../app/config'
+import { CONFIG } from '../../app/config.js'
 
 export class Button {
   constructor (app) {
     this.app = app
     this.initButton()
+    this.isDestroyed = false
   }
 
   initButton () {
@@ -28,17 +29,31 @@ export class Button {
     this.container.buttonMode = true
 
     this.eventListeners()
+    this.addKeyboardListener()
 
     this.app.stage.addChild(this.container)
   }
 
   eventListeners () {
-    this.container.on('pointerover', () => this.onPointOver())
+    this.container.on('pointerover', () => this.onPointerOver())
     this.container.on('pointerout', () => this.onPointerOut())
     this.container.on('pointerdown', () => this.onPointerDown())
   }
 
-  onPointOver () {
+  addKeyboardListener () {
+    this.keyHandler = (event) => {
+      if (event.key === 'Enter') this.onPointerDown()
+    }
+    window.addEventListener('keydown', this.keyHandler)
+  }
+
+  removeKeyboardListener () {
+    if (this.keyHandler) {
+      window.removeEventListener('keydown', this.keyHandler)
+    }
+  }
+
+  onPointerOver () {
     this.container.cursor = 'pointer'
     this.buttonSprite.alpha = 0.8
   }
@@ -48,18 +63,22 @@ export class Button {
   }
 
   onPointerDown () {
+    if (this.isDestroyed) return
     let scaleSpeed = 0.09
 
     const onTick = () => {
       this.container.scale.x -= scaleSpeed
       this.container.scale.y -= scaleSpeed
 
-      const checkScale = () => this.container.scale.x <= 0 || this.container.scale.y <= 0
+      const checkScale = () =>
+        this.container.scale.x <= 0 || this.container.scale.y <= 0
 
       if (checkScale()) {
         this.app.stage.removeChild(this.container)
         this.container.destroy()
         this.app.ticker.remove(onTick)
+        this.removeKeyboardListener()
+        this.isDestroyed = true
       }
     }
     this.app.ticker.add(onTick)
