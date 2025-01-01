ee594a8759a8fa1f4b7c0173641b609d8e33ee8c
Joyel Johny <joyel2johny@gmail.com>
2025-01-02 01:03:32 +0530

    added cursor hiding feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { Game } from './app/Game.js'
+import { CursorHider } from './app/utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const game = new Game()
+  const cursorHider = new CursorHider();
+
 })
