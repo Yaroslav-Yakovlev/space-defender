41cb770398e5087638178cc43bf7687cc76f8b7d
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2025-01-03 19:55:36 +0200

    test commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import jest from 'eslint-plugin-jest'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '.vscode/',
+      '.idea/',
+      '*.log',
+      'coverage/',
+    ],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -11,7 +20,21 @@ export default [
         module: 'readonly',
         __dirname: 'readonly'
       }
+    }
+  },
+  {
+    files: ['*.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
     },
+    plugins: {
+      jest
+    },
+    rules: {
+      ...jest.configs.recommended.rules
+    }
   },
   pluginJs.configs.recommended
 ]
