af24c50a763e623b46832b1d87ac4f4ab42f2a2d
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2025-01-04 13:24:32 +0200

    Added Jest. Implement jest config. Wrote test to BulletsCounter

---
+++ src/tests/ui/bulletsCounter.test.js
@@ -0,0 +1,32 @@
+import { Application } from 'pixi.js'
+import { BulletsCounter } from '../../ui/bulletsCounter/BulletsCounter.js'
+import { expect, jest, it, describe, beforeEach } from '@jest/globals'
+
+describe('BulletsCounter', () => {
+  let app
+  let bulletsCounter
+
+  beforeEach(() => {
+    app = new Application()
+    app.stage = { addChild : jest.fn() }
+
+    bulletsCounter = new BulletsCounter(app, 10, 10)
+  })
+
+  it('should initializes correctly', () => {
+    expect(bulletsCounter.bulletsLeft).toBe(10)
+    expect(bulletsCounter.bulletsAmount).toBe(10)
+    expect(bulletsCounter.text.text).toBe('Bullets: 10 / 10')
+  })
+
+  it('should updates the number of remaining bullets', () => {
+    bulletsCounter.update(8)
+    expect(bulletsCounter.bulletsLeft).toBe(8)
+    expect(bulletsCounter.text.text).toBe('Bullets: 8 / 10')
+  })
+
+  it('method update should not change bullets amount', () => {
+    bulletsCounter.update(5)
+    expect(bulletsCounter.bulletsAmount).toBe(10)
+  })
+})
--- src/ui/bulletsCounter/BulletsCounter.js
+++ src/ui/bulletsCounter/BulletsCounter.js
@@ -1,5 +1,5 @@
 import { Text } from 'pixi.js'
-import { bulletsCounterStyle } from './bulletsCounterStyle'
+import { bulletsCounterStyle } from './bulletsCounterStyle.js'
 
 export class BulletsCounter {
   constructor (app, bulletsLeft, bulletsAmount) {
@@ -7,14 +7,15 @@ export class BulletsCounter {
     this.bulletsLeft = bulletsLeft
     this.bulletsAmount = bulletsAmount
 
-    this.text = new Text(`Bullets: ${this.bulletsLeft} / ${this.bulletsAmount}`, bulletsCounterStyle)
+    this.text = new Text(`Bullets: ${this.bulletsLeft} / ${this.bulletsAmount}`,
+      bulletsCounterStyle)
 
     this.text.x = 20
     this.text.y = 20
     this.app.stage.addChild(this.text)
   }
 
-  update(bulletsLeft) {
+  update (bulletsLeft) {
     this.bulletsLeft = bulletsLeft
     this.text.text = `Bullets: ${this.bulletsLeft} / ${this.bulletsAmount}`
   }
